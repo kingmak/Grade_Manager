150d82e2899f148dea68bc1c6d06bc82bbdd95ad
King Mak <kingmak@hotmail.com>
2014-06-15 21:58:44 -0500

    Create Struc.cpp

---
+++ Struc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+void PrintName(struct Name Person);
+
+struct Name {
+	
+   string FirstName;
+   string LastName;
+};
+ 
+int main() {
+	
+	struct Name Person;
+	
+	Person.FirstName = "Safiullah";
+	Person.LastName = "Imtiaz";
+	
+	PrintName(Person);	
+}
+void PrintName(struct Name Person) {
+	
+   cout << "First Name: " << Person.FirstName << endl;
+   cout << "Last Name: " << Person.LastName << endl;
+}
